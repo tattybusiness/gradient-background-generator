a895f41f66672de1bc63f027868b197544b4498f
tattybusiness <tattybusinessonly@gmail.com>
2021-06-18 13:30:11 +0100

    files uploaded

---
+++ main.js
@@ -0,0 +1,13 @@
+var css = document.querySelector("h3");
+var colour1 = document.querySelector("#colour1");
+var colour2 = document.querySelector("#colour2");
+var body = document.getElementById("gradient");
+
+function gradient() {
+    body.style.background = "linear-gradient(to right, " + colour1.value + "," + colour2.value + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+colour1.addEventListener("input", gradient);
+colour2.addEventListener("input", gradient);
